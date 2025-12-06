cd14e5b74a51cd08fc3a807fd5631fe8653e6dfb
Braden Walker <bradencodes@gmail.com>
2025-12-05 23:29:22 -0800

    add function to parse id ranges

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -1,4 +1,20 @@
+use std::ops::{Range, RangeInclusive};
+
 pub fn solve() {
     let input = include_str!("input.txt");
 
+    let id_ranges = parse_id_ranges(input);
+}
+
+fn parse_id_ranges(input: &str) -> Vec<RangeInclusive<i32>> {
+    input
+        .split(",")
+        .map(|range_string| {
+            let (start, end) = range_string.split_once("-").unwrap();
+            let start: i32 = start.parse().unwrap();
+            let end: i32 = end.parse().unwrap();
+
+            start..=end
+        })
+        .collect()
 }
