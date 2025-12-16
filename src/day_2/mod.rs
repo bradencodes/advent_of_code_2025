d1d5e017ea1d2d36ef54cbe59fdcf66ae53b2639
Braden Walker <bradencodes@gmail.com>
2025-12-16 18:00:57 +0100

    completed day 2 part 2

--- src/day_2/mod.rs
+++ src/day_2/mod.rs
@@ -1,12 +1,25 @@
 use std::ops::RangeInclusive;
 
-pub fn solve(input: &str) -> u64 {
+pub fn solve_part_1(input: &str) -> u64 {
     let id_ranges = parse_id_ranges(input);
+
+    fn get_is_id_valid_part_1(id: u64) -> bool {
+        let id = id.to_string();
+        if id.len() % 2 != 0 {
+            return true;
+        };
+        let (first_half, last_half) = id.split_at(id.len() / 2);
+        if first_half == last_half {
+            return false;
+        };
+
+        true
+    }
 
     let mut total_of_invalid_ids = 0;
     for id_range in id_ranges {
         for id in id_range {
-            let is_id_valid = get_is_id_valid(id);
+            let is_id_valid = get_is_id_valid_part_1(id);
             if !is_id_valid {
                 total_of_invalid_ids += id
             };
@@ -16,17 +29,56 @@ pub fn solve(input: &str) -> u64 {
     total_of_invalid_ids
 }
 
-fn get_is_id_valid(id: u64) -> bool {
-    let id = id.to_string();
-    if id.len() % 2 != 0 {
-        return true;
-    };
-    let (first_half, last_half) = id.split_at(id.len() / 2);
-    if first_half == last_half {
-        return false;
-    };
+fn get_is_id_valid_part_2(id: u64) -> bool {
+    let id_string = id.to_string();
+    let id_length = id_string.len();
+    let largest_step_size = id_length / 2;
+    for step_size in (1..=largest_step_size).rev() {
+        // an invalid id must have a sequence that repeats a whole number of times
+        if id_length % step_size != 0 {
+            continue;
+        }
+
+        let sequence_repeats_at_step_size = {
+            let num_of_chunks = id_length / step_size;
+
+            (0..step_size).all(|offset| {
+                let are_all_nums_same_in_each_chunk_at_offset = {
+                    let first_chunk_num = id_string.get(offset..=offset).unwrap();
+                    (1..=num_of_chunks).all(|chunk| {
+                        let index_to_check = (chunk - 1) * step_size + offset;
+                        let current_chunk_num =
+                            id_string.get(index_to_check..=index_to_check).unwrap();
+                        current_chunk_num == first_chunk_num
+                    })
+                };
+
+                are_all_nums_same_in_each_chunk_at_offset
+            })
+        };
+
+        if sequence_repeats_at_step_size {
+            return false;
+        };
+    }
 
     true
+}
+
+pub fn solve_part_2(input: &str) -> u64 {
+    let id_ranges = parse_id_ranges(input);
+
+    let mut total_of_invalid_ids = 0;
+    for id_range in id_ranges {
+        for id in id_range {
+            let is_id_valid = get_is_id_valid_part_2(id);
+            if !is_id_valid {
+                total_of_invalid_ids += id
+            }
+        }
+    }
+
+    total_of_invalid_ids
 }
 
 fn parse_id_ranges(input: &str) -> Vec<RangeInclusive<u64>> {
@@ -44,13 +96,31 @@ fn parse_id_ranges(input: &str) -> Vec<RangeInclusive<u64>> {
 }
 
 #[cfg(test)]
-mod tests {
+mod part_1_tests {
     use super::*;
 
     #[test]
-    fn solve_gives_correct_solution() {
+    fn solve_part_1_gives_correct_solution() {
         let test_input = include_str!("test_input.txt");
-        let solution = solve(test_input);
+        let solution = solve_part_1(test_input);
         assert_eq!(solution, 1227775554);
+    }
+}
+
+#[cfg(test)]
+mod part_2_tests {
+    use super::*;
+
+    #[test]
+    fn get_is_id_valid_part_2_works() {
+        let invalid_id = 1188511885;
+        assert_eq!(get_is_id_valid_part_2(invalid_id), false);
+    }
+
+    #[test]
+    fn solve_part_2_gives_correct_solution() {
+        let test_input = include_str!("test_input.txt");
+        let solution = solve_part_2(test_input);
+        assert_eq!(solution, 4174379265);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use advent_of_code_2025::day_2;
 
 fn main() {
     let input = include_str!("./day_2/input.txt");
-    let day_2_solution = day_2::solve(input);
-    println!("day 1 solution: {day_2_solution}");
+    let day_2_part_2_solution = day_2::solve_part_2(input);
+    println!("day_2_part_2_solution: {day_2_part_2_solution}");
 }
