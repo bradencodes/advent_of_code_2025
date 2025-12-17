742deebd33ff462b1a5a0b8f3bed02fda3b89a58
Braden Walker <bradencodes@gmail.com>
2025-12-17 17:02:06 +0100

    subopimal solution for day 3 part 1

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -1,4 +1,4 @@
-fn find_largest_joltage(bank: &str) -> u32 {
+fn find_largest_joltage_errant(bank: &str) -> u32 {
     // initialize the largest_first_num and largest_second_num to the second to last
     // and last digits in the bank, respectively.
     let mut largest_first_num: u32 = 0;
@@ -28,6 +28,43 @@ fn find_largest_joltage(bank: &str) -> u32 {
     largest_joltage
 }
 
+fn find_largest_joltage(bank: &str) -> u32 {
+    let mut largest_first_num = 0;
+    let mut largest_first_num_index = 0;
+    for (i, char) in bank.split_at(bank.len() - 1).0.chars().rev().enumerate() {
+        let digit = char.to_digit(10).unwrap();
+        if digit >= largest_first_num {
+            largest_first_num = digit;
+            largest_first_num_index = bank.len() - i - 2;
+        }
+    }
+
+    let mut largest_second_num = 0;
+    for char in bank.split_at(largest_first_num_index + 1).1.chars() {
+        let digit = char.to_digit(10).unwrap();
+        if digit > largest_second_num {
+            largest_second_num = digit
+        };
+    }
+
+    let largest_joltage = largest_first_num * 10 + largest_second_num;
+    largest_joltage
+}
+
+pub fn solve_part_1(input: &str) -> u32 {
+    let mut total_joltage = 0;
+    let banks = input.split("\n");
+    for bank in banks {
+        if bank.len() < 1 {
+            continue;
+        }
+
+        total_joltage += find_largest_joltage(bank);
+    }
+
+    total_joltage
+}
+
 #[cfg(test)]
 mod part_1_tests {
     use super::*;
@@ -38,5 +75,25 @@ mod part_1_tests {
         assert_eq!(find_largest_joltage("811111111111119"), 89);
         assert_eq!(find_largest_joltage("234234234234278"), 78);
         assert_eq!(find_largest_joltage("818181911112111"), 92);
+        assert_eq!(
+            find_largest_joltage(
+                "2232133233333122223222321121432322323324333234233221423334362333113343833132233313523312322224432234"
+            ),
+            85
+        );
+        assert_eq!(find_largest_joltage("911829"), 99);
+        assert_eq!(find_largest_joltage("4111151111"), 51);
+    }
+
+    #[test]
+    fn solve_part_1_test_works() {
+        let input = include_str!("test_input.txt");
+        assert_eq!(solve_part_1(input), 357);
+    }
+
+    #[test]
+    fn solve_part_1_works() {
+        let input = include_str!("input.txt");
+        assert_eq!(solve_part_1(input), 17311);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use advent_of_code_2025::day_2;
+use advent_of_code_2025::day_3;
 
 fn main() {
-    let input = include_str!("./day_2/input.txt");
-    let day_2_part_2_solution = day_2::solve_part_2(input);
-    println!("day_2_part_2_solution: {day_2_part_2_solution}");
+    let input = include_str!("./day_3/input.txt");
+    let day_3_part_1_solution = day_3::solve_part_1(input);
+    println!("day_3_part_1_solution: {day_3_part_1_solution}");
 }
