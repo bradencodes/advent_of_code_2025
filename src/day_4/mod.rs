ddf057a55f94fbfa20733c219ffa2b88a9997f7e
Braden Walker <bradencodes@gmail.com>
2025-12-27 09:33:46 -0800

    function to check if roll is accessbile is working

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -1,36 +1,94 @@
-fn check_if_roll_is_accessible(diagrid: [[char]], index: usize) -> bool {
-    let row_length = diagrid[0].len()
-    let is_top_left_open = {
-        let top_left_index = index - row_length - 1;
-        let is_top_left_off_edge =
-            top_left_index < index || top_left_index % row_length >= index % row_length;
+type RowIndex = usize;
+type ColumnIndex = usize;
+type GridIndex = (RowIndex, ColumnIndex);
+
+const ROLL: char = '@';
+const ACCESS_LIMIT: u8 = 4;
+
+fn check_if_roll_is_accessible(diagrid: &[Vec<char>], grid_index: GridIndex) -> bool {
+    let column_length = diagrid.len();
+    let row_length = diagrid[0].len();
+
+    let is_top_edge = grid_index.0 == 0;
+    let is_bottom_edge = grid_index.0 == column_length - 1;
+    let is_left_edge = grid_index.1 == 0;
+    let is_right_edge = grid_index.1 == row_length - 1;
+
+    let mut adjacent_roll_count: u8 = 0;
+
+    let is_roll_top_left =
+        !is_top_edge && !is_left_edge && diagrid[grid_index.0 - 1][grid_index.1 - 1] == ROLL;
+    if is_roll_top_left {
+        adjacent_roll_count += 1
     };
 
-    true
+    let is_roll_top = !is_top_edge && diagrid[grid_index.0 - 1][grid_index.1] == ROLL;
+    if is_roll_top {
+        adjacent_roll_count += 1
+    };
+
+    let is_roll_top_right =
+        !is_top_edge && !is_right_edge && diagrid[grid_index.0 - 1][grid_index.1 + 1] == ROLL;
+    if is_roll_top_right {
+        adjacent_roll_count += 1
+    };
+
+    let is_roll_left = !is_left_edge && diagrid[grid_index.0][grid_index.1 - 1] == ROLL;
+    if is_roll_left {
+        adjacent_roll_count += 1
+    };
+
+    let is_roll_right = !is_right_edge && diagrid[grid_index.0][grid_index.1 + 1] == ROLL;
+    if is_roll_right {
+        adjacent_roll_count += 1
+    };
+
+    let is_roll_bottom_left =
+        !is_bottom_edge && !is_left_edge && diagrid[grid_index.0 + 1][grid_index.1 - 1] == ROLL;
+    if is_roll_bottom_left {
+        adjacent_roll_count += 1
+    };
+
+    let is_roll_bottom = !is_bottom_edge && diagrid[grid_index.0 + 1][grid_index.1] == ROLL;
+    if is_roll_bottom {
+        adjacent_roll_count += 1
+    };
+
+    let is_roll_bottom_right =
+        !is_bottom_edge && !is_right_edge && diagrid[grid_index.0 + 1][grid_index.1 + 1] == ROLL;
+    if is_roll_bottom_right {
+        adjacent_roll_count += 1
+    };
+
+    adjacent_roll_count < ACCESS_LIMIT
 }
 
 #[cfg(test)]
 mod part_1_tests {
     use super::*;
 
-    // ....\n
-    // .@@@\n
-    // .@@@\n
-    // .@@@\n
-    // ....\n
-
     #[test]
     fn check_if_roll_is_accessible_works() {
-        let diagram_string = "....\n.@@@\n.@@@\n.@@@\n....\n";
-        let diagrid = diagram_string.split('\n').map(|row| row.chars()).collect();
-        assert_eq!(check_if_roll_is_accessible(diagrid, 5), true);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 6), false);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 7), true);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 9), false);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 10), false);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 11), false);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 13), true);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 14), false);
-        assert_eq!(check_if_roll_is_accessible(diagrid, 15), true);
+        let diagram_string = "....\n.@@@\n.@@@\n.@@@\n....";
+        let diagrid: Vec<Vec<char>> = diagram_string
+            .split('\n')
+            .map(|row| row.chars().collect())
+            .collect();
+
+        // ....\n
+        // .@@@\n
+        // .@@@\n
+        // .@@@\n
+        // ....\n
+
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (1, 1)), true);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (1, 2)), false);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (1, 3)), true);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (2, 1)), false);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (2, 2)), false);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (2, 3)), false);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (3, 1)), true);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (3, 2)), false);
+        assert_eq!(check_if_roll_is_accessible(&diagrid, (3, 3)), true);
     }
 }
