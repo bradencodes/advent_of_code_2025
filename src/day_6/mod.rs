47dfeb6ed9b798f58bd2255e427c0e11c4e882cc
Braden Walker <bradencodes@gmail.com>
2026-01-11 11:48:26 -0800

    solve day 6 part 1

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -4,26 +4,55 @@ pub mod part_1 {
         Multiply,
     }
 
-    struct Problem {
-        numbers: Vec<u64>,
-        operator: Operator,
-    }
-
     fn calculate_grand_total(input: &str) -> u64 {
-        let results = {
-            let problem_grid: Vec<Vec<&str>> = input
-                .split("\n")
-                .filter(|string| !string.is_empty())
-                .map(|line| line.split_whitespace().collect())
-                .collect();
+        let mut grand_total = 0;
 
-            // TODO: Iterate through columns of problem_grid backwards from the end to
-            // get operator and perform operation on numbers in problem.
-        };
+        let problem_grid: Vec<Vec<&str>> = input
+            .split("\n")
+            .filter(|string| !string.is_empty())
+            .map(|line| line.split_whitespace().collect())
+            .collect();
 
-        let grand_total = results.iter().sum();
+        // TODO: Iterate through columns of problem_grid backwards from the end to
+        // get operator and perform operation on numbers in problem.
+        let column_length = problem_grid.len();
+        let row_length = problem_grid[0].len();
+
+        for row_index in 0..row_length {
+            let mut operator: Option<Operator> = None;
+            let mut total = 0;
+            for column_index in (0..column_length).rev() {
+                let problem_part = problem_grid[column_index][row_index];
+                if column_index == column_length - 1 {
+                    match problem_part {
+                        "+" => {
+                            operator = Some(Operator::Add);
+                            total = 0;
+                        }
+                        "*" => {
+                            operator = Some(Operator::Multiply);
+                            total = 1;
+                        }
+                        _ => panic!("'{problem_part}' is not a valid operator"),
+                    }
+                } else {
+                    let number: u64 = problem_part.parse().expect("part should be a number");
+                    match operator {
+                        Some(Operator::Add) => total += number,
+                        Some(Operator::Multiply) => total *= number,
+                        None => unreachable!(),
+                    }
+                }
+            }
+
+            grand_total += total;
+        }
 
         grand_total
+    }
+
+    pub fn solve(input: &str) -> u64 {
+        calculate_grand_total(input)
     }
 
     #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use advent_of_code_2025::day_5;
+use advent_of_code_2025::day_6::part_1;
 
 fn main() {
-    let input = include_str!("./day_5/input.txt");
-    let day_5_part_2_solution = day_5::solve_part_2(input);
-    println!("day_5_part_2_solution: {day_5_part_2_solution}");
+    let input = include_str!("./day_6/input.txt");
+    let day_6_part_1_solution = part_1::solve(input);
+    println!("day_6_part_1_solution: {day_6_part_1_solution}");
 }
