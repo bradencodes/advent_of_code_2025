b9862545093409770c44e6a8cae357b5482d89c8
Braden Walker <bradencodes@gmail.com>
2026-01-12 17:23:23 -0800

    WIP: parse input and comment plan for day 6 part 2

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -1,8 +1,10 @@
+enum Operator {
+    Add,
+    Multiply,
+}
+
 pub mod part_1 {
-    enum Operator {
-        Add,
-        Multiply,
-    }
+    use super::Operator;
 
     fn calculate_grand_total(input: &str) -> u64 {
         let mut grand_total = 0;
@@ -13,7 +15,7 @@ pub mod part_1 {
             .map(|line| line.split_whitespace().collect())
             .collect();
 
-        // TODO: Iterate through columns of problem_grid backwards from the end to
+        // Iterate through columns of problem_grid backwards from the end to
         // get operator and perform operation on numbers in problem.
         let column_length = problem_grid.len();
         let row_length = problem_grid[0].len();
@@ -63,6 +65,58 @@ pub mod part_1 {
         fn calculate_grand_total_works() {
             let input = include_str!("./test_input.txt");
             assert_eq!(calculate_grand_total(input), 4277556);
+        }
+    }
+}
+
+pub mod part_2 {
+    use super::Operator;
+
+    fn calculate_grand_total(input: &str) -> u64 {
+        let mut grand_total = 0;
+
+        let (raw_number_char_grid, operators) = {
+            let mut raw_lines: Vec<&str> =
+                input.split("\n").filter(|line| !line.is_empty()).collect();
+            let operators: Vec<Operator> = raw_lines
+                .pop()
+                .unwrap()
+                .split_whitespace()
+                .map(|symbol| match symbol {
+                    "+" => Operator::Add,
+                    "*" => Operator::Multiply,
+                    _ => panic!(""),
+                })
+                .collect();
+
+            let raw_number_char_grid: Vec<Vec<char>> = raw_lines
+                .iter()
+                .map(|line| line.chars().collect())
+                .collect();
+
+            (raw_number_char_grid, operators)
+        };
+
+        // TODO: iterate through raw_number_char_grid by going through each column starting at the top,
+        // building the current number until the bottom of the column is reached. Once it's reached,
+        // perform the operation for that column. If a column of all spaces is read, then add the
+        // local total to the grand total and reset the local total for the next problem.
+
+        grand_total
+    }
+
+    pub fn solve(input: &str) -> u64 {
+        calculate_grand_total(input)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn calculate_grand_total_works() {
+            let input = include_str!("./test_input.txt");
+            assert_eq!(calculate_grand_total(input), 3263827);
         }
     }
 }
