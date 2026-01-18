bbaa8d7d8ddcec99263673ced68956585f4c37f4
Braden Walker <bradencodes@gmail.com>
2026-01-18 06:30:43 -0800

    WIP: Basic logic filled out but not passing test

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -102,6 +102,44 @@ pub mod part_2 {
         // perform the operation for that column. If a column of all spaces is read, then add the
         // local total to the grand total and reset the local total for the next problem.
 
+        let grid_column_length = raw_number_char_grid.len();
+        let grid_row_length = raw_number_char_grid[0].len();
+
+        let mut operators_iter = operators.iter();
+        let mut current_operation = operators_iter.next().unwrap();
+        let mut current_total = match current_operation {
+            Operator::Add => 0,
+            Operator::Multiply => 1,
+        };
+        for row_index in 0..grid_row_length {
+            let mut current_number = String::new();
+
+            for column_index in 0..grid_column_length {
+                let grid_char = raw_number_char_grid[column_index][row_index];
+                if grid_char.is_numeric() {
+                    current_number.push(grid_char);
+                }
+                let is_end_of_column = column_index == grid_column_length;
+                if is_end_of_column {
+                    let is_end_of_problem = current_number.is_empty();
+                    if is_end_of_problem {
+                        grand_total += current_total;
+                        current_operation = operators_iter.next().unwrap();
+                        current_total = match current_operation {
+                            Operator::Add => 0,
+                            Operator::Multiply => 1,
+                        };
+                    } else {
+                        let final_number: u64 = current_number.parse().unwrap();
+                        match current_operation {
+                            Operator::Add => current_total += final_number,
+                            Operator::Multiply => current_total *= final_number,
+                        };
+                    }
+                }
+            }
+        }
+
         grand_total
     }
 
